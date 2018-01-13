1f48592ad1196f2176a6fc059c1c6203315c19f5
Vedire <prudhvi.vedire@capgemini.com>
2018-01-13 17:38:23 -0500

    initial commit

---
+++ src/main/java/SpringMVCRestfulExample/RestFulWebservice/App.java
@@ -0,0 +1,13 @@
+package SpringMVCRestfulExample.RestFulWebservice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
